24d241204ee3192fdde518db5045ec4b0aaa21a4
alejandro <alejo4373@gmail.com>
2019-05-13 17:30:50 -0400

    Updates

---
+++ precache-manifest.4e335ed6a03fd52f730e9680086a5d54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46fdc7ca3b7aa72e0af01e7c74d3a9d4",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "04cd38f36606f1d6461c",
+    "url": "/react-gh-pages/static/css/main.c09d8ee7.chunk.css"
+  },
+  {
+    "revision": "5ded595fca651bc21ea5",
+    "url": "/react-gh-pages/static/js/2.e0237689.chunk.js"
+  },
+  {
+    "revision": "04cd38f36606f1d6461c",
+    "url": "/react-gh-pages/static/js/main.7cad1e76.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  }
+]);
